b84ca5335194747b211caccb9a47cfdeb1080ab4
damalis <erdalaltin@gmail.com>
2024-07-04 20:05:42 +0300

    fixed database authentication plugin error

--- install.sh
+++ install.sh
@@ -221,13 +221,13 @@ echo "======================================================================="
 echo ""
 echo ""
 echo "======================================================================="
-echo "| Installing Docker Compose v2.23.3..."
+echo "| Installing Docker Compose v2.27.2..."
 echo "======================================================================="
 echo ""
 sleep 2
 
 sudo mkdir -p /usr/local/lib/docker/cli-plugins
-sudo curl -SL "https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/lib/docker/cli-plugins/docker-compose
+sudo curl -SL "https://github.com/docker/compose/releases/download/v2.27.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/lib/docker/cli-plugins/docker-compose
 sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
 
 echo ""
@@ -251,6 +251,7 @@ echo ""
 echo "Done ✓"
 echo "======================================================================="
 
+clear
 ##########
 # Setup project variables
 ##########
@@ -363,8 +364,7 @@ done
 echo "Ok."
 
 which_db=""
-db_authentication_plugin="mysql_native_password"
-db_authentication_password="USING PASSWORD('"$pma_password"')"
+db_authentication_password=$pma_password
 db_package_manager="apt-get update \&\& apt-get install -y gettext-base"
 db_admin_commandline="mariadb-admin"
 db_connect_extension="mariadb"
@@ -374,8 +374,6 @@ do
 	which_db=$db
 	if [ $REPLY -eq 2 ]
 	then
-		db_authentication_plugin="caching_sha2_password"
-		db_authentication_password="BY '"$pma_password"'"
 		db_package_manager="microdnf install -y gettext"
 		db_admin_commandline="mysqladmin"
 		db_connect_extension="mysqli"
@@ -414,9 +412,7 @@ cp ./database/phpmyadmin/sql/create_tables.sql.template.example ./database/phpmy
 
 cp env.example .env
 
-sed -i 's/db_authentication_plugin/'$db_authentication_plugin'/' ./database/phpmyadmin/sql/create_tables.sql.template
 sed -i "s/db_authentication_password/${db_authentication_password}/" ./database/phpmyadmin/sql/create_tables.sql.template
-sed -i 's/db_authentication_plugin/'$db_authentication_plugin'/' .env
 sed -i "s|db_package_manager|${db_package_manager}|" .env
 sed -i 's/db_admin_commandline/'$db_admin_commandline'/' .env
 sed -i 's/db_connect_extension/'$db_connect_extension'/' .env
